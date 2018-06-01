4a7634c87eac9ec28b9586d72f812f781d37d5c6
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2018-06-01 15:53:44 +0200

    Added books page

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 5000;
+const bookRouter = express.Router();
+
 app.use(morgan("tiny"));
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(
@@ -25,8 +27,58 @@ app.use(
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+const books = [
+  {
+    title: "War and Peace",
+    genre: "Historical Fiction",
+    author: "Lev Tolstoy",
+    read: false
+  },
+  {
+    title: "Les Miserables",
+    genre: "Historical Fiction",
+    author: "Victor Hugo",
+    read: false
+  },
+  {
+    title: "The Time Machine",
+    genre: "Science Fiction",
+    author: "H. G. Wells",
+    read: false
+  },
+  {
+    title: "The Dark World",
+    genre: "Fantasy",
+    author: "Kenneth Grahame",
+    read: false
+  }
+];
+
+bookRouter.route("/").get((req, res) => {
+  res.render("books", {
+    title: "The Library",
+    nav: [
+      { link: "/books", title: "Books" },
+      { link: "/authors", title: "Authors" }
+    ],
+    books
+  });
+});
+
+bookRouter.route("/single").get((req, res) => {
+  res.send("hello single book");
+});
+
+app.use("/books", bookRouter);
+
 app.get("/", (req, res) => {
-  res.render("index", { title: "The Library", list: ["a", "b"] });
+  res.render("index", {
+    title: "The Library",
+    nav: [
+      { link: "/books", title: "Books" },
+      { link: "/authors", title: "Authors" }
+    ]
+  });
 
   // res.sendFile(path.join(__dirname, "views/index.html"));
   // this also works:
