6949c7cf4143f61048093b1f6ae411d7f563f8a3
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2018-06-01 11:40:51 +0200

    Added Bootstrap

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ var express = require("express");
 var chalk = require("chalk");
 var debug = require("debug")("app");
 var morgan = require("morgan");
+var path = require("path");
 
 var app = express();
 
 app.use(morgan("tiny"));
-
+app.use(express.static(path.join(__dirname, "/public")));
 app.get("/", function(req, res) {
-  res.send("Hello from the library app");
+  res.sendFile(path.join(__dirname, "views/index.html"));
+  //this also works:
+  //   res.sendFile(path.join(__dirname, "/views/", "/index.html/"));
 });
 
 app.listen(3000, function() {
